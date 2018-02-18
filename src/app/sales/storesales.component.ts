325eef776689874931ace53f3e0e9b73cb225579
WhiteSnow <rgb92911.0.3@gmail.com>
2018-02-18 16:08:02 +0000

    button color fixed

--- src/app/sales/storesales.component.ts
+++ src/app/sales/storesales.component.ts
@@ -12,39 +12,42 @@ export class StoreComponent {
   years = years
   months = months
   current_target = ''
+  btn_outline_class = 'btn-outline-primary'
+  btn_fill_class = 'btn-primary'
   constructor(public elRef: ElementRef) { }
   actionChanged ($event, action) {
     this.current_target = action
     if (action === 'actuals') {
-      console.log(action)
-      $('.actuals_btn').removeClass('btn-outline-success')
-      $('.actuals_btn').addClass('btn-success')
-      $('.targets_btn').removeClass('btn-primary')
+      this.btn_outline_class = 'btn-outline-success'
+      this.btn_fill_class = 'btn-success'
+      $('.actuals_btn').addClass(this.btn_fill_class)
+      $('.actuals_btn').removeClass(this.btn_outline_class)
       $('.targets_btn').addClass('btn-outline-primary')
-      $event.target.classList.add('btn-success')
+      $('.targets_btn').removeClass('btn-primary')
     } else {
-      console.log(action)
+      this.btn_outline_class = 'btn-outline-primary'
+      this.btn_fill_class = 'btn-primary'
       $('.actuals_btn').addClass('btn-outline-success')
       $('.actuals_btn').removeClass('btn-success')
-      $('.targets_btn').addClass('btn-primary')
-      $('.targets_btn').removeClass('btn-outline-primary')
+      $('.targets_btn').addClass(this.btn_fill_class)
+      $('.targets_btn').removeClass(this.btn_outline_class)
     }
   }
   storeChanged ($event, store) {
-    $('.store_btn').addClass('btn-outline-primary')
-    $('.store_btn').removeClass('btn-primary')
-    $event.target.classList.remove('btn-outline-primary')
-    $event.target.classList.add('btn-primary')
+    $('.store_btn').addClass(this.btn_outline_class)
+    $('.store_btn').removeClass(this.btn_fill_class)
+    $event.target.classList.remove(this.btn_outline_class)
+    $event.target.classList.add(this.btn_fill_class)
   }
   yearChanged ($event, year) {
-    $('.year_btn').addClass('btn-outline-primary')
-    $('.year_btn').removeClass('btn-primary')
-    $event.target.classList.remove('btn-outline-primary')
-    $event.target.classList.add('btn-primary')
+    $('.year_btn').addClass(this.btn_outline_class)
+    $('.year_btn').removeClass(this.btn_fill_class)
+    $event.target.classList.remove(this.btn_outline_class)
+    $event.target.classList.add(this.btn_fill_class)
   }
   monthChanged ($event, month) {
-    $event.target.classList.toggle('btn-outline-primary')
-    $event.target.classList.toggle('btn-primary')
+    $event.target.classList.toggle(this.btn_outline_class)
+    $event.target.classList.toggle(this.btn_fill_class)
   }
   saveTargets () {
     alert('target saved!')
